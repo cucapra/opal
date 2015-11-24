5bc67e39692326df10a3131b7d5c8aa449b2f5bd
Adrian Sampson <adrian@radbox.org>
2015-11-24 12:49:26 -0800

    A few comments

--- src/opal.ts
+++ src/opal.ts
@@ -1,8 +1,11 @@
 'use strict';
 
+// The base class for events emitted inside hypothetical worlds to be consumed
+// by the OPAL runtime.
 class Message {
 }
 
+// A communication channel between hypothetical worlds and their parents.
 class Weight<T> {
   values: Map<number, T>;
   constructor(public ctx: Context) {
@@ -14,10 +17,16 @@ class Weight<T> {
   }
 }
 
+// Type aliases for the coroutines that form the basis of hypothetical worlds.
+type WorldCoroutine = IterableIterator<Message>;
+type WorldCoroutineFunc = () => WorldCoroutine;
+
+// A World is a dynamic instance of a hypothetical block. It mainly consists of
+// a generator (coroutine) object.
 class World {
   constructor(public ctx: Context,
               public id: number,
-              public coroutine: Iterable<Message>) {}
+              public coroutine: WorldCoroutine) {}
 }
 
 class Context {
@@ -29,7 +38,7 @@ class Context {
   }
 
   // Create a new world.
-  hypothetical(f: GeneratorFunction) {
+  hypothetical(f: WorldCoroutineFunc) {
     let id = this.next_world_id;
     ++this.next_world_id;
     let world = new World(this, id, f());
