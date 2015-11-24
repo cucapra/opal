569e121db58b86734c2a2d00d2c2770883a6b54a
Adrian Sampson <adrian@radbox.org>
2015-11-24 13:54:34 -0800

    Marginally more useful example

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,8 +1,17 @@
 opal(function* (ctx) {
+  // A communication channel between us an subworlds.
   let w = ctx.weight();
-  let hyp = ctx.hypothetical(function* (ctx) {
-    yield ctx.set(w, 2);
+  
+  // Spawn some hypothetical worlds.
+  let hyp1 = ctx.hypothetical(function* (ctx) {
+    yield ctx.set(w, 2);  // ideal syntax: `w = 2`
   });
-  let v = yield ctx.get(w, hyp);
-  console.log("got", v, "from subworld");
+  let hyp2 = ctx.hypothetical(function* (ctx) {
+    yield ctx.set(w, 8);
+  });
+  
+  // Collect the weights from the subworlds.
+  let v1 = yield ctx.get(w, hyp1);  // ideal syntax: w[hyp1]
+  let v2 = yield ctx.get(w, hyp2);
+  console.log("values from subworlds:", v1, v2);
 });
