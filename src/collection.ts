55aa4b50a06041d39cd69a6f2eac9bacd59afaa3
Adrian Sampson <adrian@radbox.org>
2015-12-09 14:19:44 -0800

    Refactor again to separate nodes from ops

--- src/collection.ts
+++ src/collection.ts
@@ -2,7 +2,57 @@ abstract class CollectionNode<T> {
   constructor(
     public parent: CollectionNode<T>
   ) {}
-  abstract view(): Set<T>;
+
+  abstract log(): CollectionOperation<T>[];
+
+  view(): Set<T> {
+    var out: Set<T> = new Set();
+    for (let op of this.log()) {
+      op.apply(out);
+    }
+    return out;
+  }
+}
+
+abstract class CollectionOperation<T> {
+  abstract apply(set: Set<T>): void;
+}
+
+class AddOperation<T> extends CollectionOperation<T> {
+  constructor(
+    public value: T
+  ) {
+    super();
+  }
+
+  apply(set: Set<T>) {
+    set.add(this.value);
+  }
+}
+
+class DeleteOperation<T> extends CollectionOperation<T> {
+  constructor(
+    public value: T
+  ) {
+    super();
+  }
+
+  apply(set: Set<T>) {
+    set.delete(this.value);
+  }
+}
+
+class OperationNode<T> extends CollectionNode<T> {
+  constructor(
+    public parent: CollectionNode<T>,
+    public operation: CollectionOperation<T>
+  ) {
+    super(parent);
+  }
+
+  log(): CollectionOperation<T>[] {
+    return this.parent.log().concat(this.operation);
+  }
 }
 
 class EmptyNode<T> extends CollectionNode<T> {
@@ -10,38 +60,8 @@ class EmptyNode<T> extends CollectionNode<T> {
     super(null);
   }
 
-  view(): Set<T> {
-    return new Set();
-  }
-}
-
-class AddNode<T> extends CollectionNode<T> {
-  constructor(
-    parent: CollectionNode<T>,
-    public value: T)
-  {
-    super(parent);
-  };
-
-  view(): Set<T> {
-    let out = this.parent.view();
-    out.add(this.value);
-    return out;
-  }
-}
-
-class DeleteNode<T> extends CollectionNode<T> {
-  constructor(
-    parent: CollectionNode<T>,
-    public value: T
-  ) {
-    super(parent);
-  };
-
-  view(): Set<T> {
-    let out = this.parent.view();
-    out.delete(this.value);
-    return out;
+  log(): CollectionOperation<T>[] {
+    return [];
   }
 }
 
@@ -53,7 +73,22 @@ class MergeNode<T> extends CollectionNode<T> {
     super(parent);
   }
 
-  view(): Set<T> {
+  log(): CollectionOperation<T>[] {
+    // Find the closest common ancestor.
+    let ancestors: Set<CollectionNode<T>> = new Set();
+    let overlay_ancestor = this.overlay;
+    do {
+      ancestors.add(overlay_ancestor);
+      overlay_ancestor = overlay_ancestor.parent;
+    } while (overlay_ancestor !== null && overlay_ancestor !== this.parent);
+
+    let parent_ancestor = this.parent;
+    while (parent_ancestor !== null && !ancestors.has(parent_ancestor)) {
+      parent_ancestor = parent_ancestor.parent;
+    }
+
+    console.assert(parent_ancestor !== null, "parent and overlay are unrelated");
+
     // TODO
     return null;
   }
