b8b3e329b34513b278221f04646fc2b5132d2807
Adrian Sampson <adrian@radbox.org>
2015-12-09 12:51:08 -0800

    Introduce a MergeNode

--- src/collection.ts
+++ src/collection.ts
@@ -1,16 +1,26 @@
 abstract class CollectionNode<T> {
+  constructor(
+    public parent: CollectionNode<T>
+  ) {}
   abstract view(): Set<T>;
 }
 
 class EmptyNode<T> extends CollectionNode<T> {
+  constructor() {
+    super(null);
+  }
+
   view(): Set<T> {
     return new Set();
   }
 }
 
 class AddNode<T> extends CollectionNode<T> {
-  constructor(public parent: CollectionNode<T>, public value: T) {
-    super();
+  constructor(
+    parent: CollectionNode<T>,
+    public value: T)
+  {
+    super(parent);
   };
 
   view(): Set<T> {
@@ -21,13 +31,30 @@ class AddNode<T> extends CollectionNode<T> {
 }
 
 class DeleteNode<T> extends CollectionNode<T> {
-  constructor(public parent: CollectionNode<T>, public value: T) {
-    super();
+  constructor(
+    parent: CollectionNode<T>,
+    public value: T
+  ) {
+    super(parent);
   };
 
   view(): Set<T> {
     let out = this.parent.view();
     out.delete(this.value);
     return out;
+  }
+}
+
+class MergeNode<T> extends CollectionNode<T> {
+  constructor(
+    parent: CollectionNode<T>,
+    public overlay: CollectionNode<T>
+  ) {
+    super(parent);
+  }
+
+  view(): Set<T> {
+    // TODO
+    return null;
   }
 }
