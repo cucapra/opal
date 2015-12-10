4f2c006ffe309a16b901cf6bd4d6af60c4777dd5
Adrian Sampson <adrian@radbox.org>
2015-12-10 11:39:38 -0800

    Namespace the collection stuff

--- src/collection.ts
+++ src/collection.ts
@@ -1,86 +1,90 @@
-abstract class CollectionNode<T> {
-  constructor(
-    public parent: CollectionNode<T>
-  ) {}
+module Collection {
 
-  abstract log(): CollectionOperation<T>[];
+  abstract class Node<T> {
+    constructor(
+      public parent: Node<T>
+    ) {}
 
-  view(): Set<T> {
-    var out: Set<T> = new Set();
-    for (let op of this.log()) {
-      op.apply(out);
+    abstract log(): Operation<T>[];
+
+    view(): Set<T> {
+      var out: Set<T> = new Set();
+      for (let op of this.log()) {
+        op.apply(out);
+      }
+      return out;
     }
-    return out;
-  }
-}
-
-abstract class CollectionOperation<T> {
-  abstract apply(set: Set<T>): void;
-}
-
-class AddOperation<T> extends CollectionOperation<T> {
-  constructor(
-    public value: T
-  ) {
-    super();
   }
 
-  apply(set: Set<T>) {
-    set.add(this.value);
-  }
-}
-
-class DeleteOperation<T> extends CollectionOperation<T> {
-  constructor(
-    public value: T
-  ) {
-    super();
+  abstract class Operation<T> {
+    abstract apply(set: Set<T>): void;
   }
 
-  apply(set: Set<T>) {
-    set.delete(this.value);
-  }
-}
+  class Add<T> extends Operation<T> {
+    constructor(
+      public value: T
+    ) {
+      super();
+    }
 
-class OperationNode<T> extends CollectionNode<T> {
-  constructor(
-    public parent: CollectionNode<T>,
-    public operation: CollectionOperation<T>
-  ) {
-    super(parent);
-  }
-
-  log(): CollectionOperation<T>[] {
-    return this.parent.log().concat(this.operation);
-  }
-}
-
-class EmptyNode<T> extends CollectionNode<T> {
-  constructor() {
-    super(null);
+    apply(set: Set<T>) {
+      set.add(this.value);
+    }
   }
 
-  log(): CollectionOperation<T>[] {
-    return [];
+  class Delete<T> extends Operation<T> {
+    constructor(
+      public value: T
+    ) {
+      super();
+    }
+
+    apply(set: Set<T>) {
+      set.delete(this.value);
+    }
   }
-}
 
-function merge<T>(base: CollectionNode<T>, overlay: CollectionNode<T>) {
-    // The first step is to accumulate the *entire* set of ancestors of the
-    // base so we can check membership when traversing the overlay's ancestry.
-    let base_ancestors: Set<CollectionNode<T>> = new Set();
-    let base_ancestor = this.base;
-    do {
-      base_ancestors.add(base_ancestor);
-      base_ancestor = base_ancestor.parent;
-    } while (base_ancestor !== null && base_ancestor !== this.parent);
+  class OperationNode<T> extends Node<T> {
+    constructor(
+      public parent: Node<T>,
+      public operation: Operation<T>
+    ) {
+      super(parent);
+    }
 
-    // Next, get the overlay's log *up to but not including* the closest
-    // common ancestor.
-    let log_suffix = overlay.log(base_ancestors);
+    log(): Operation<T>[] {
+      return this.parent.log().concat(this.operation);
+    }
+  }
 
-    // TODO Check safety: the two nodes need to be related (have some common
-    // ancestor). We also need to check for conflicting concurrent operations.
+  class EmptyNode<T> extends Node<T> {
+    constructor() {
+      super(null);
+    }
 
-    // TODO
+    log(): Operation<T>[] {
+      return [];
+    }
+  }
+
+  function merge<T>(base: Node<T>, overlay: Node<T>) {
+      // The first step is to accumulate the *entire* set of ancestors of the
+      // base so we can check membership when traversing the overlay's ancestry.
+      let base_ancestors: Set<Node<T>> = new Set();
+      let base_ancestor = this.base;
+      do {
+        base_ancestors.add(base_ancestor);
+        base_ancestor = base_ancestor.parent;
+      } while (base_ancestor !== null && base_ancestor !== this.parent);
+
+      // Next, get the overlay's log *up to but not including* the closest
+      // common ancestor.
+      let log_suffix = overlay.log(base_ancestors);
+
+      // TODO Check safety: the two nodes need to be related (have some common
+      // ancestor). We also need to check for conflicting concurrent operations.
+
+      // TODO
+  }
+
 }
