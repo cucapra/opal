3ba288c58b27f508e80cbb81ffa163cecf4a5dd4
Adrian Sampson <adrian@radbox.org>
2016-01-26 14:46:42 -0800

    Demonstrate adding to calendar collection

--- src/schedule.ts
+++ src/schedule.ts
@@ -34,6 +34,18 @@ opal(function* (ctx) {
 
   // Experimenting with the calendar API.
   let events: Collection<Calendar.Event> = yield Calendar.events();
+  console.log("calendar events:");
+  for (let e of ctx.view(events)) {
+    console.log(e.subject);
+  }
+
+  // Add an event.
+  let hyp4 = ctx.hypothetical(function* (ctx): IterableIterator<Message> {
+    let e = new Calendar.Event("Exciting Meeting!", "12:00", "1:00");
+    ctx.add(events, e);
+  });
+  yield ctx.commit(hyp4);
+  console.log("events now:");
   for (let e of ctx.view(events)) {
     console.log(e.subject);
   }
