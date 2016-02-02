0ef23c0c730b82b6862768824e30618fabb31921
Adrian Sampson <adrian@radbox.org>
2016-02-01 16:49:51 -0800

    Start OPAL-specific tests file
    Not quite working yet: need to remove the redundancies between the two files.

---
+++ test/test_opal.ts
@@ -0,0 +1,29 @@
+/// <reference path="../src/opal.ts" />
+/// <reference path="../typings/main.d.ts" />
+
+let assert = require('assert');
+let test = require('tape');
+
+test('weights', function (t: any) {
+  opal(function* (ctx) {
+    // A communication channel between us an subworlds.
+    let w = ctx.weight();
+
+    // Spawn some hypothetical worlds.
+    let hyp1 = ctx.hypothetical(function* (ctx) {
+      yield ctx.set(w, 2);  // ideal syntax: `w = 2`
+    });
+    let hyp2 = ctx.hypothetical(function* (ctx) {
+      yield ctx.set(w, 8);
+    });
+
+    // Collect the weights from the subworlds.
+    let v1 = yield ctx.get(w, hyp1);  // ideal syntax: w[hyp1]
+    let v2 = yield ctx.get(w, hyp2);
+
+    t.assert(v1 === 2);
+    t.assert(v2 === 8);
+
+    t.end();
+  });
+});
