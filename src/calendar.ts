e76d4fa1ce67ba2c3f16f09aecc212db9e6a2cea
Adrian Sampson <adrian@radbox.org>
2016-02-16 13:58:14 -0800

    Actually add events

--- src/calendar.ts
+++ src/calendar.ts
@@ -35,6 +35,27 @@ module Calendar {
     );
   }
 
+  function addEvent(event: Event, cbk: (error: any, result: any) => void) {
+    // TODO: Remove copypasta.
+    let home = getUserHome();
+    let email = fs.readFileSync(path.join(home, ".opal.email.txt"));
+    let token = fs.readFileSync(path.join(home, ".opal.token.txt"));
+
+    // Set the API endpoint to use the v2.0 endpoint
+    outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+    // Set the anchor mailbox to the user's SMTP address
+    outlook.base.setAnchorMailbox(email);
+    outlook.base.setPreferredTimeZone('Pacific Standard Time');
+
+    // TODO: Is this redundancy necessary?
+    var user = {
+      email: email,
+      timezone: 'Pacific Standard Time',
+    };
+
+    outlook.calendar.createEvent({token: token, user: user, event: event.toOffice()}, cbk);
+  }
+
   function toDate(d: string | Date): Date {
     if (d instanceof Date) {
       return d;
@@ -43,6 +64,23 @@ module Calendar {
     }
   }
 
+  function pad0(n: number): string {
+    if (n < 10) {
+      return '0' + n;
+    } else {
+      return n.toString();
+    }
+  }
+
+  function dateToOffice(d: Date): string {
+    return d.getFullYear() +
+      '-' + pad0(d.getMonth() + 1)
+      '-' + pad0(d.getDate()) +
+      'T' + pad0(d.getHours()) +
+      ':' + pad0(d.getMinutes());
+  }
+
+  // Represents a single calendar event.
   export class Event {
     constructor(
       public subject: string,
@@ -52,6 +90,26 @@ module Calendar {
       this.start = toDate(start);
       this.end = toDate(end);
     };
+
+    // Convert to the Office API's JSON representation.
+    toOffice() {
+      return {
+        'Subject': this.subject,
+        'Body': {
+          'ContentType': 'HTML',
+          'Content': '',  // TODO
+        },
+        'Start': {
+          'DateTime': dateToOffice(this.start),
+          'TimeZone': 'Pacific Standard Time', // TODO
+        },
+        'End': {
+          'DateTime': dateToOffice(this.end),
+          'TimeZone': 'Pacific Standard Time', // TODO
+        },
+        'Attendees': [] as any[],
+      };
+    }
   }
 
   function array_remove<T>(a: T[], x: T) {
@@ -67,7 +125,11 @@ module Calendar {
       let events: Event[] = Array.from(old.view());
       for (let op of ops) {
         if (op instanceof PSet.Add) {
-          console.log("TODO: add", op.value);
+          addEvent(op.value, (error, result) => {
+            if (error) {
+              console.log("error adding event:", error);
+            }
+          });
           events.push(op.value);
         } else if (op instanceof PSet.Delete) {
           console.log("TODO: delete", op.value);
