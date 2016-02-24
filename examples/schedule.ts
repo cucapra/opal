bf2743e753e3f7d33853f1b78342ee5cafc727b3
Adrian Sampson <adrian@radbox.org>
2016-02-24 15:08:07 -0800

    Light rearrangement

--- examples/schedule.ts
+++ examples/schedule.ts
@@ -12,7 +12,7 @@ function copyDate(d: Date) {
 function* slots(start: Date, end: Date, incrementMinutes: number) {
   // Clone the start date for our iterator.
   let current = copyDate(start);
-  
+
   // Increment the date repeatedly.
   while (current < end) {
     yield current;
@@ -26,38 +26,37 @@ opal(async function (ctx) {
                           title: string, minutes: number)
   {
     let conflicts = ctx.weight<number>();
-    
+
     let worlds = ctx.explore(range, start => async function (ctx) {
       // Try adding the event to the calendar.
       let end = copyDate(start);
       end.setMinutes(start.getMinutes() + minutes);
       let evt = new Event(title, start, end);
       ctx.add(events, evt);
-      
+
       // Check for conflicts.
       ctx.set(conflicts, 1);  // TODO
     });
-    
+
     // Find the best time.
     return await ctx.minimize(worlds, conflicts, 100);
   }
 
   // Get my calendar.
   let events: Calendar = await getEvents(ctx);
-  
+
   // Schedule a meeting.
-  let startRange = slots(
-    new Date("February 3, 2014 08:00:00"),
-    new Date("February 3, 2014 17:00:00"),
-    30
-  );
   let world = await schedule(
     events,
-    startRange,
+    slots(
+      new Date("February 3, 2014 08:00:00"),
+      new Date("February 3, 2014 17:00:00"),
+      30
+    ),
     "Exciting Meeting!",
     60
   );
-  
+
   // Affect the real world.
   await ctx.commit(world);
 });
