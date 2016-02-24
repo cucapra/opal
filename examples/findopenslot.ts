d9397cf04d626a76dc7d1457b1df70afec106871
slbird <slbird@microsoft.com>
2016-02-24 14:42:37 -0800

    sarahsfile now compiling

--- examples/findopenslot.ts
+++ examples/findopenslot.ts
@@ -1,23 +1,30 @@
-/// <reference path="../src/opal.ts" />
-/// <reference path="../src/calendar.ts" />
 'use strict';
 
-findopenslot(async function (ctx) {
+import {opal, Context } from '../src/opal';
+import { Event, Calendar, getEvents } from '../src/calendar';
+
+async function findopenslot(ctx: Context) {
   // Experimenting with the calendar API.
-  let events: Calendar.Calendar = await Calendar.events(ctx);
-  console.log("calendar events:");
+  let events: Calendar = await getEvents(ctx);
+    console.log("calendar events:");
   for (let e of ctx.view(events)) {
     console.log(e.subject);
   }
 
   // Add an event.
   let hyp4 = ctx.hypothetical(async function (ctx) {
-    let e = new Calendar.Event("Exciting Meeting!", "12:00", "1:00");
+    let e = new Event(
+        "Exciting Meeting!",
+        new Date("February 3, 2014 12:00:00"),
+        new Date("February 3, 2014 13:00:00")
+        );
     ctx.add(events, e);
   });
   await ctx.commit(hyp4);
-  console.log("events now:");
+console.log("events now:");
   for (let e of ctx.view(events)) {
-    console.log(e.subject);
+    console.log(e.subject, e.start, e.end);
   }
-});
+}
+
+opal(findopenslot);
