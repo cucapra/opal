70f0c20f74c95f94118b909dcccf22ba21eeaee1
Adrian Sampson <adrian@radbox.org>
2016-04-09 15:09:26 -0700

    Authorize chat sessions!

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,8 +1,13 @@
 import * as botbuilder from 'botbuilder';
 import {Auth} from '../src/office';
 let restify = require('restify');
+import * as crypto from 'crypto';
 
 const AUTH_CBK = "http://localhost:8191/authorize";
+
+function randomString() {
+  return crypto.randomBytes(64).toString('hex');
+}
 
 // The bot interaction (CLI for now).
 let bot = new botbuilder.TextBot();
@@ -16,22 +21,43 @@ bot.add('/', function (session) {
   }
 });
 
-// A "waterfall" dialog for getting authorized.
-bot.add('/login', [
-  function (session) {
-    session.send("Let's get you signed in.");
-    let authurl = Auth.getAuthUrl(AUTH_CBK, "SEEKRIT");
-    session.send("Please follow this URL: " + authurl);
-  }
-]);
+// This shouldn't be a global. :(
+let authRequests = {};
+
+// A dialog for requesting authorization.
+bot.add('/login', function (session) {
+  session.send("Let's get you signed in.");
+  let authKey = randomString();
+  authRequests[authKey] = session;
+  let authurl = Auth.getAuthUrl(AUTH_CBK, authKey);
+  session.send("Please follow this URL: " + authurl);
+});
+
+// When authorization succeeds.
+bot.add('/loggedin', function (session) {
+  session.send("That worked! You're now signed in as " +
+    session.userData['email'] + ".");
+});
 
 bot.listenStdin();
 
 // Handle an authentication.
+function sessionAuthenticated(session: botbuilder.Session,
+  token: string, email: string)
+{
+  session.userData['token'] = token;
+  session.userData['email'] = email;
+  session.beginDialog('/loggedin');
+}
+
 function authenticated(token: string, email: string, state: string) {
-  console.log(token);
-  console.log(email);
-  console.log(state);
+  let session = authRequests[state];
+  if (session) {
+    sessionAuthenticated(session, token, email);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // The Web server.
@@ -44,9 +70,12 @@ server.get('/authorize', function (req, res, next) {
     if (error) {
       res.send("Sorry! We couldn't sign you in. " + error.message);
     } else {
-      res.send("Thanks! You're all signed in. You can close this tab.");
       let pair = Auth.parseToken(token);
-      authenticated(pair[0], pair[1], state);
+      if (authenticated(pair[0], pair[1], state)) {
+        res.send("Thanks! You're all signed in. You can close this tab.");
+      } else {
+        res.send("We don't have a record of this authentication request.");
+      }
     }
   });
   return next();
