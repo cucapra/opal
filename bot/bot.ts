1ac634d2d65707b5138ee94a1f6273ebf17e8f0d
Adrian Sampson <adrian@radbox.org>
2016-04-09 13:50:32 -0700

    Start a web server for the bot

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,8 +1,18 @@
 import * as botbuilder from 'botbuilder';
+let restify = require('restify');
 
+// The bot interaction (CLI for now).
 let bot = new botbuilder.TextBot();
 bot.add('/', function (session) {
   session.send("yo");
 });
-
 bot.listenStdin();
+
+// The Web server.
+let server = restify.createServer();
+server.get('/authorize', function (req, res, next) {
+  return next();
+});
+server.listen(8191, function () {
+  console.log('server listening at %s', server.url);
+});
