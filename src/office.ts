397c8f1af8f220b7142eae88ef2570fc83497171
Adrian Sampson <adrian@radbox.org>
2016-04-09 13:27:07 -0700

    Transfer over some Office auth business

--- src/office.ts
+++ src/office.ts
@@ -117,3 +117,77 @@ function dateToOfficeDateTimeTimezone(d: Date): any {
     "TimeZone" : "Pacific Standard Time"
   };
 }
+
+/**
+ * Office authentication tools.
+ */
+namespace Auth {
+  var credentials = {
+      clientID: "944881d4-f34a-4e54-b102-586c11f0e49f",
+      clientSecret: "dMGSZmPP1prDatq5AvcjqCJ",
+      site: "https://login.microsoftonline.com/common",
+      authorizationPath: "/oauth2/v2.0/authorize",
+      tokenPath: "/oauth2/v2.0/token"
+  }
+  var oauth2 = require("simple-oauth2")(credentials);
+
+  var redirectUri = "http://localhost:8191/authorize";
+
+  // scopes
+  var scopes = [
+      "openid",
+      "profile",  // Required to get user's email address.
+      "https://outlook.office.com/mail.read",
+      "https://outlook.office.com/calendars.readwrite",
+  ];
+
+  export function getAuthUrl() {
+      var returnVal = oauth2.authCode.authorizeURL({
+          redirect_uri: redirectUri,
+          scope: scopes.join(" ")
+      });
+      return returnVal;
+  }
+
+  export function getTokenFromCode(auth_code: string, callback: (response: any, error: any, token: string) => void, response: any) {
+      var token: any;
+      oauth2.authCode.getToken({
+          code: auth_code,
+          redirect_uri: redirectUri,
+          scope: scopes.join(" ")
+      }, function (error: any, result: string) {
+          if (error) {
+              callback(response, error, null);
+          }
+          else {
+              token = oauth2.accessToken.create(result);
+              callback(response, null, token);
+          }
+      });
+  }
+
+  function getEmailFromIdToken(id_token: string) {
+      // JWT is in three parts, separated by a '.'
+      var token_parts = id_token.split('.');
+
+      // Token content is in the second part, in urlsafe base64
+      var encoded_token = new Buffer(token_parts[1].replace("-", "_").replace("+", "/"), 'base64');
+
+      var decoded_token = encoded_token.toString();
+
+      var jwt = JSON.parse(decoded_token);
+
+      // Email is in the preferred_username field
+      return jwt.preferred_username
+  }
+
+  /**
+   * Given an OAuth token object, return the token string and the user's
+   * email address. Together, these can be used for future API requests.
+   */
+  export function parseToken(token: any): [string, string] {
+    var t = token.token.access_token;
+    var em = getEmailFromIdToken(token.token.id_token);
+    return [t, em];
+  }
+}
