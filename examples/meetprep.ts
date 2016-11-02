31ec1a4f66c0256128138af387a08eea940db5a0
Adrian Sampson <adrian@radbox.org>
2016-11-02 13:53:01 -0400

    Slightly more structure

--- examples/meetprep.ts
+++ examples/meetprep.ts
@@ -2,6 +2,10 @@ import {opal, Context} from '../src/opal';
 import {Event} from '../src/calendar';
 import {User} from '../src/office';
 
+class Document {
+  constructor(public url: string) {}
+}
+
 function query_event(): Event {
   return new Event(
     "test",
@@ -11,6 +15,33 @@ function query_event(): Event {
   );
 }
 
+function get_docs(): Document[] {
+  return [];
+}
+
+function relevance(event: Event, doc: Document) {
+  return 0.99;
+}
+
 opal(async function (ctx) {
-  let e = query_event();
+  // Start with a "key" event and the entire set of documents.
+  let event = query_event();
+  let all_docs = get_docs();
+  let threshold = 0.8;
+
+  // A collection where we'll store the documents we found.
+  let relevant_docs = ctx.collection();
+
+  // Try one document at a time.
+  let worlds = ctx.explore(all_docs, doc => async function (ctx) {
+    let rel = relevance(event, doc);
+    if (rel > threshold) {
+      ctx.add(relevant_docs, doc);
+    }
+  });
+
+  // Join all the worlds together?
+  for (let world of worlds) {
+    ctx.commit(world);
+  }
 });
