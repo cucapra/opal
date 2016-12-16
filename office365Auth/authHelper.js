bc4470734061062a71e88384947ecbcb3e04a4fc
Adrian Sampson <adrian@radbox.org>
2016-12-16 15:43:33 -0500

    Update for simple-oauth2 API changes

--- office365Auth/authHelper.js
+++ office365Auth/authHelper.js
@@ -1,11 +1,15 @@
 ﻿var credentials = {
-    clientID: "944881d4-f34a-4e54-b102-586c11f0e49f",
-    clientSecret: "dMGSZmPP1prDatq5AvcjqCJ",
-    site: "https://login.microsoftonline.com/common",
-    authorizationPath: "/oauth2/v2.0/authorize",
-    tokenPath: "/oauth2/v2.0/token"
+    client: {
+        id: "944881d4-f34a-4e54-b102-586c11f0e49f",
+        secret: "dMGSZmPP1prDatq5AvcjqCJ",
+    },
+    auth: {
+        tokenHost: "https://login.microsoftonline.com",
+        tokenPath: "common/oauth2/v2.0/token",
+        authorizePath: "common/oauth2/v2.0/authorize",
+    },
 }
-var oauth2 = require("simple-oauth2")(credentials);
+var oauth2 = require("simple-oauth2").create(credentials);
 
 var redirectUri = "http://localhost:8191/authorize";
 
@@ -18,7 +22,7 @@ var scopes = [
 ];
 
 function getAuthUrl() {
-    var returnVal = oauth2.authCode.authorizeURL({
+    var returnVal = oauth2.authorizationCode.authorizeURL({
         redirect_uri: redirectUri,
         scope: scopes.join(" ")
     });
@@ -30,7 +34,7 @@ exports.getAuthUrl = getAuthUrl;
 
 function getTokenFromCode(auth_code, callback, response) {
     var token;
-    oauth2.authCode.getToken({
+    oauth2.authorizationCode.getToken({
         code: auth_code,
         redirect_uri: redirectUri,
         scope: scopes.join(" ")
@@ -52,14 +56,14 @@ exports.getTokenFromCode = getTokenFromCode;
 function getEmailFromIdToken(id_token) {
     // JWT is in three parts, separated by a '.'
     var token_parts = id_token.split('.');
-    
+
     // Token content is in the second part, in urlsafe base64
     var encoded_token = new Buffer(token_parts[1].replace("-", "_").replace("+", "/"), 'base64');
-    
+
     var decoded_token = encoded_token.toString();
-    
+
     var jwt = JSON.parse(decoded_token);
-    
+
     // Email is in the preferred_username field
     return jwt.preferred_username
 }
